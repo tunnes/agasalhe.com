69f7f4bac9208c08425288ed97006a5fd78116b2
Ayrton Felipe <ayrton.tunes@outlook.com>
2017-07-16 19:53:43 +0000

    Created basical estruture for application routes  - Brand new controllers with native CI methods.  - Generated routes with operation in controller.  - Implements function to show views to operate application.  - Comment about estruture and our page features.

---
+++ application/views/contact.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Contact</title>
+	</head>
+	<body>
+		<h1>To do... Contact page</h1>
+	</body>
+</html>
