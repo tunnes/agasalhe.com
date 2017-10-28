b8a2dd85d161cf144dff45633577924bb8612fe1
Ayrton Felipe <ayrton.tunes@outlook.com>
2017-10-28 05:05:19 +0000

    Create register image feature on application.
     - Controller to show and set images.
     - Table on database.
     - Create an folter to collections and database versions.

---
+++ application/controllers/API/Image.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Image extends CI_Controller {
+	
+	public function __construct() 
+    {
+        parent::__construct();
+        $this->load->model('image_model');
+      
+    }
+    
+	public function index($ID)
+	{
+		header( "Content-type: image/jpeg");
+		echo $this->image_model->get_image($ID);
+	}
+	
+	public function register()
+	{
+		$this->image_model->set_image($_FILES['image'], "Uma bela image", 10);
+	}
+
+}
