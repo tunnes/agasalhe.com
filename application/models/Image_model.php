b7d089ae126c00eede71abcfd42a2cf47a288134
Tarcisio Lima <tarcisio.bomdebola@bol.com.br>
2017-10-28 21:33:42 +0000

    Renaming column names.

--- application/models/Image_model.php
+++ application/models/Image_model.php
@@ -12,11 +12,11 @@ class Image_model extends CI_Model {
 		if (move_uploaded_file($IMG_FILE['tmp_name'], $imageName)) {
 			$data = array
             (
-                'image_file' => file_get_contents($imageName),
-                'image_alt' => $ALT,
-                'image_reference' => $REF
+                'file' => file_get_contents($imageName),
+                'alt' => $ALT,
+                'item_id' => $REF
             );
-            return $this->db->insert('image', $data);
+            return $this->db->insert('images', $data);
 			unlink($imageName);
 		}
 		else{
@@ -28,9 +28,9 @@ class Image_model extends CI_Model {
     public function get_image($ID)
     {
         $this->db->select('*');
-        $this->db->from('image');
-        $this->db->where('image_id', $ID);
-        $image = $this->db->get()->row()->image_file;
+        $this->db->from('images');
+        $this->db->where('id', $ID);
+        $image = $this->db->get()->row()->file;
         // header( "Content-type: image/jpeg"); 
         return $image;
     }
@@ -38,8 +38,8 @@ class Image_model extends CI_Model {
     public function get_references($REF)
     {
         $this->db->select('*');
-        $this->db->from('image');
-        $this->db->where('image_reference', $REF);
+        $this->db->from('images');
+        $this->db->where('item_id', $REF);
         $references = $this->db->get()->result_array();
         return $references;
     }
