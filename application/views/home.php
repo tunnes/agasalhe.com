37c0404dc32f9ef907cf245295a726e29f2fa1f9
Tarcisio Lima <tarcisio.bomdebola@bol.com.br>
2017-10-29 13:40:14 +0000

    A little bug fix about responsivity.

--- application/views/home.php
+++ application/views/home.php
@@ -36,6 +36,7 @@
         <!-- Another control and meta-tags -->
         <link rel="shortcut icon" href="/application/assets/img/favicon.ico" type="image/x-icon">
         <link rel="icon" href="/favicon.ico" type="/application/assets/imgx-icon">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta charset="UTF-8">
         <title>Swapage</title>
     </head>
