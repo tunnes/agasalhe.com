827db625b3ab4018d5305dd1421db612c471676f
Ayrton Felipe <ayrton.tunes@outlook.com>
2017-12-02 12:16:08 +0000

    Multiple implementations in the system for project closure.

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	define('ENVIRONMENT', $_SERVER['SERVER_ADDR'] == '192.185.223.59' ? 'production' : 'development');
 
 /*
  *---------------------------------------------------------------
